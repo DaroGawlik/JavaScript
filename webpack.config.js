75438fce8ede6a8e40eaa68711a47183ac810ab9
Darek <gawlikdaarek@gmail.com>
2023-06-25 11:53:00 +0200

    webkpack: added production build

--- webpack.config.js
+++ webpack.config.js
@@ -2,36 +2,41 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin')
 
-module.exports = {
-	mode: 'development',
-	entry: {
-		index: path.resolve(__dirname, './src/index.js'),
-	},
-	output: {
-		path: path.resolve(__dirname, './'),
-		filename: 'build/[name].budle.js',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, './src/template.html'),
-		}),
-		new HtmlWebpackInlineSVGPlugin({
-			runPreEmit: true,
-			inlineAll: true,
-		}),
-	],
-
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.svg$/,
-				type: 'asset/source',
-			},
+// eslint-disable-next-line arrow-parens
+module.exports = arg => {
+	const isProduction = arg === 'production'
+	const isDevelopment = !isProduction
+	return {
+		mode: arg,
+		entry: {
+			index: path.resolve(__dirname, './src/index.js'),
+		},
+		output: {
+			path: path.resolve(__dirname, './'),
+			filename: 'build/[name].budle.js',
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: path.resolve(__dirname, './src/template.html'),
+			}),
+			new HtmlWebpackInlineSVGPlugin({
+				runPreEmit: true,
+				inlineAll: true,
+			}),
 		],
-	},
-	watch: true,
+
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+				{
+					test: /\.svg$/,
+					type: 'asset/source',
+				},
+			],
+		},
+		watch: isDevelopment,
+	}
 }
